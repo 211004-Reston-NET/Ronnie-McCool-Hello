c86ea9ac1816d6f4bcdacbb0a6d046da04da2dd1
Ron McCool <ronmccool32@gmail.com>
2021-10-05 16:12:40 -0500

    anotherPush

--- c#/HelloWorld/Program.cs
+++ c#/HelloWorld/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-
+//This is a comment
+/*
+Comment 
+MultiLines
+use PascalCae naming convention AllWordsStartWithCapitals
+use camelCase for naming fields firstWordIsCapitialized
+*/
 namespace HelloWorld
 {
     class Program
     {
+        /*
+           Main method is the first method that will be called run Compiler looks for main
+           static means I don't have to instanciate an class to use a static method
+        */
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Program test=new Program();
+            test.Example();
+            /*If method is not static then the class has to be instanciated*/
+            Program.Example2();
+            System.Console.WriteLine(Program.ExampleInt());
+            House house=new House();
+        
+            house.Owner="McCool";
+            System.Console.WriteLine( house.ToString()+"  "+house.Owner);
+        }
+        public void Example(){
+
+        }
+        public static void Example2(){
+            System.Console.WriteLine("Your number is: ");
+
+        }
+         public static int ExampleInt(){
+             return 10;
+
         }
     }
 }
---
+++ firstNameLastName-c#Hello/FirstNameLastName/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstNameLastName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What your name");
+            string name=Console.ReadLine();
+            System.Console.WriteLine("Your name is: "+name);
+        }
+    }
+}
