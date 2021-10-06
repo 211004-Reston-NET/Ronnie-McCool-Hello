c09bcb1f0fa83f0972d84eadd085cfd77cdd9de3
Ron McCool <ronmccool32@gmail.com>
2021-10-06 12:01:52 -0500

    12:00NOW

---
+++ c#/HelloWorld/CollectionFunctions.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+namespace CollectionFunctions{
+
+
+    
+        public class Collection{
+
+        //Generic Collectiom
+        List<string> strings = new List<string>();
+        
+        public Collection(){
+
+        }
+
+        public Collection(string name){
+            strings.Add(name);
+        }
+
+        public void CollectionMain(){
+
+        }
+
+    }
+
+}
